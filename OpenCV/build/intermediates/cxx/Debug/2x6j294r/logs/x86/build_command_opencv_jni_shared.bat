98d15b7d3bc34bf07c9848a0aca006351c29f1ff
anik2644 <anik11556@gmail.com>
2025-09-04 17:41:41 +0600

    build gradle updated

--- OpenCV/build/intermediates/cxx/Debug/2x6j294r/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\User\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\Bio-SDK-2-Morpho\\OpenCV\\.cxx\\Debug\\2x6j294r\\x86" ^
-  opencv_jni_shared
